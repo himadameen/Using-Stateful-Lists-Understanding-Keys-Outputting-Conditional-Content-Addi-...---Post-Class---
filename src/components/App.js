652adb70128c1bfb996cb91b6ab6153dc5e70ab3
Himmad Ameen <93907951+himadameen@users.noreply.github.com>
2022-05-25 16:43:14 +0530

    my changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 const App = () => {
-  
+  const [name, setname] = useState(false);
+  function change() {
+    setname(!name);
+  }
   return (
     <div id="main">
-
+      <h1 id="marco-polo">{name ? "Polo" : "Marco"}</h1>
+      <button id="marco-polo-toggler" onClick={change}>
+        {name ? "Marco" : "Polo"}
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
